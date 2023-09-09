34a42934203fad39938cc758354874261bece8f5
cookie4566 <kennedyc416@gmail.com>
2023-09-09 09:11:59 -0400

    Learning how tkvideoplayer works in relation to tk.
    Adding video play back features

--- pyvideo.py
+++ pyvideo.py
@@ -10,10 +10,14 @@ root.title = ('pymp4')
 # Window size
 root.geometry('1000x500')
 vid = TkinterVideo(master = root, scaled = True)
+vid.load(r"[ViViD REAL] Gundam AGE 3 OP.mp4")
+vid.pack()
 
 open = Button(root, text = 'open', command= root.destroy)
 
 open.pack(fill= BOTH)
+
+vid.play()
 
 
 # intializes widow as an output
