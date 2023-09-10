935b86113ea63b93a0adbced7820b5149994ad7b
cookie4566 <kennedyc416@gmail.com>
2023-09-10 18:08:33 -0400

    added copyrighted video to test tkVideoPlayer and removed the open button in favor for a pulldown menu.

--- pyvideo.py
+++ pyvideo.py
@@ -9,16 +9,31 @@ root = Tk()
 root.title = ('pymp4')
 # Window size
 root.geometry('1000x500')
+# Initilazing TkinterVideo and loading a test video/loading video
 vid = TkinterVideo(master = root, scaled = True)
 vid.load(r"[ViViD REAL] Gundam AGE 3 OP.mp4")
 vid.pack()
 
-open = Button(root, text = 'open', command= root.destroy)
 
-open.pack(fill= BOTH)
+# Buttons used for testing tkVideoPlayer
 
+play = Button(root, text = "Play", command = vid.play)
+pause = Button(root,text = "pause" ,command = vid.pause)
+
+play.pack()
+pause.pack()
+
+#menu
+menu = Menu(root)
+fmenu = Menu(menu, tearoff=0)
+menu.add_cascade(menu = fmenu, label = 'placeholder')
+fmenu.add_command(label = "open",command = root.destroy)
+
+
+# Starts video playback
 vid.play()
 
 
 # intializes widow as an output
+root.config(menu=menu)
 root.mainloop()
