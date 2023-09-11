0f354aeb9896b61d73476fdfc876dbf90ec1d175
cookie4566 <kennedyc416@gmail.com>
2023-09-11 14:12:40 -0400

    Moved around the 'pause', 'play', and the video by using the pack() function
    Better looking GUI.

--- pyvideo.py
+++ pyvideo.py
@@ -3,31 +3,42 @@
 # created 9-7-2023
 from tkinter import *
 from tkVideoPlayer import TkinterVideo
+from tkinter.filedialog import *
+
+# Function to open mp4 files
+def open():
+    file = askopenfile(mode='r', filetypes=[('mp4')])
+    if file is None:
+        vid
+        vid.load()
 
 # Creates window and titles the window
 root = Tk()
 root.title = ('pymp4')
+frame = Frame(root)
+frame.pack()
 # Window size
 root.geometry('1000x500')
+
 # Initilazing TkinterVideo and loading a test video/loading video
 vid = TkinterVideo(master = root, scaled = True)
 vid.load(r"[ViViD REAL] Gundam AGE 3 OP.mp4")
-vid.pack()
-
+vid.pack(fill='both',expand='true')
 
 # Buttons used for testing tkVideoPlayer
 
 play = Button(root, text = "Play", command = vid.play)
 pause = Button(root,text = "pause" ,command = vid.pause)
 
-play.pack()
-pause.pack()
+play.pack(side='left')
+pause.pack(side='right')
+
 
 #menu
 menu = Menu(root)
 fmenu = Menu(menu, tearoff=0)
 menu.add_cascade(menu = fmenu, label = 'placeholder')
-fmenu.add_command(label = "open",command = root.destroy)
+fmenu.add_command(label = "open",command = 'open')
 
 
 # Starts video playback
